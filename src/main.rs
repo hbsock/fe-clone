f2fd3331e58db09c659063dc551feaeb7443f7e8
Hanbin Sock <hbsock@gmail.com>
2018-09-05 19:39:48 -0400

    Add basic structs for a 2D board.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[derive(Clone)]
+enum TileType {
+    Empty,
+}
+
+#[derive(Clone)]
+struct Tile {
+    tile_type: TileType,
+}
+
+struct Board {
+    width: usize,
+    height: usize,
+    tiles: Vec<Tile>,
+}
+
+impl Board {
+    pub fn new(w: usize, h: usize) -> Board {
+        Board {
+            width: w,
+            height: h,
+            tiles: vec![
+                Tile{ tile_type: TileType::Empty }; 
+                w * h
+            ],
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
