28ab76035526fa10e715647abffa8e2ab0a6fe5e
Hanbin Sock <hbsock@gmail.com>
2018-09-09 16:01:01 -0400

    Move logic of pong example to pong.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,15 @@
 extern crate amethyst;
 
-use amethyst::input::{is_close_requested, is_key_down};
+
+//mod tile;
+//mod board;
+mod pong;
+
+use pong::Pong;
+
 use amethyst::prelude::*;
-use amethyst::renderer::{DisplayConfig, DrawFlat, Event, Pipeline,
-                         PosTex, RenderBundle, Stage, VirtualKeyCode};
-
-mod tile;
-mod board;
-
-use board::*;
-
-
-
-pub struct Pong;
-impl<'a, 'b> State<GameData<'a, 'b>> for Pong {
-    fn handle_event(&mut self, _: StateData<GameData>, event: Event) -> Trans<GameData<'a, 'b>> {
-        if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
-            Trans::Quit
-        } else {
-            Trans::None
-        }
-    }
-
-    fn update(&mut self, data: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
-        data.data.update(&data.world);
-        Trans::None
-    }
-}
+use amethyst::renderer::{DisplayConfig, DrawFlat, Pipeline,
+                         PosTex, RenderBundle, Stage};
 
 
 fn main() -> amethyst::Result<()> {
