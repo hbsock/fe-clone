89902fd0d36fe3cfebf8ff3fc29274ad5ab63c98
Hanbin Sock <hbsock@gmail.com>
2018-09-11 22:55:30 -0400

    Implement State for the board struct, and try to start Amethyst using Board instead of Pong.

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,13 @@
 use std::fmt; 
 use tile::*;
 
+use amethyst::input::{is_close_requested, is_key_down};
+use amethyst::prelude::*;
+use amethyst::renderer::{
+    Camera, Event, PngFormat, Projection, Sprite, Texture, TextureHandle,
+    VirtualKeyCode, WithSpriteRender,
+};
+
 pub struct Board {
     width: usize,
     height: usize,
@@ -58,4 +65,24 @@ impl fmt::Display for Board {
         
         Ok(())
     }
+}
+
+impl<'a, 'b> State<GameData<'a, 'b>> for Board {
+    fn handle_event(&mut self, _: StateData<GameData>, event: Event) -> Trans<GameData<'a, 'b>> {
+
+        if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+            Trans::Quit
+        } else {
+            Trans::None
+        }
+    }
+
+    fn update(&mut self, data: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+        data.data.update(&data.world);
+        Trans::None
+    }
+
+	fn on_start(&mut self, data: StateData<GameData>) {
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 extern crate amethyst;
 
 
-//mod tile;
-//mod board;
+mod tile;
+mod board;
 mod systems;
 mod pong;
 
-use pong::Pong;
+//use pong::Pong;
+use board::Board;
 
 use amethyst::prelude::*;
 use amethyst::renderer::{DisplayConfig, DrawFlat, Pipeline,
@@ -39,11 +40,11 @@ fn main() -> amethyst::Result<()> {
 
 	let game_data = GameDataBuilder::default()
 		.with_bundle(TransformBundle::new())?
-		.with_bundle(RenderBundle::new(pipe, Some(config)))?
-		.with_bundle(input_bundle)?
-		.with(systems::PaddleSystem, "paddle_system", &["input_system"]); // Add this line
+		.with_bundle(RenderBundle::new(pipe, Some(config)))?;
+		//.with_bundle(input_bundle)?
+		//.with(systems::PaddleSystem, "paddle_system", &["input_system"]); // Add this line
 
-	let mut game = Application::new("./", Pong, game_data)?;
+	let mut game = Application::new("./", Board::new(10, 10), game_data)?;
 	game.run();
 
 	Ok(())
