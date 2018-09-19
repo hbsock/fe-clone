951807490ff6556d701a6bcc156b559e64856a3c
Hanbin Sock <hbsock@gmail.com>
2018-09-18 20:02:39 -0400

    Retab main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,34 +17,34 @@ fn main() -> amethyst::Result<()> {
 
 
     // We'll put the rest of the code here.
-	amethyst::start_logger(Default::default());
+    amethyst::start_logger(Default::default());
 
     let path = "./resources/display_config.ron";
     let config = DisplayConfig::load(&path);
 
-	let pipe = Pipeline::build().with_stage(
-		Stage::with_backbuffer()
-			.clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
-			.with_pass(DrawFlat::<PosTex>::new()),
-	);
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+            .with_pass(DrawFlat::<PosTex>::new()),
+    );
+
+
+    let binding_path = format!(
+        "{}/resources/bindings_config.ron",
+        env!("CARGO_MANIFEST_DIR")
+    );
 
     use amethyst::input::InputBundle;
+    //let input_bundle = InputBundle::<String, String>::new().with_bindings_from_file(binding_path)?;
 
-	let binding_path = format!(
-		"{}/resources/bindings_config.ron",
-		env!("CARGO_MANIFEST_DIR")
-	);
+    let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(RenderBundle::new(pipe, Some(config)))?;
+        //.with_bundle(input_bundle)?
+        //.with(systems::PaddleSystem, "paddle_system", &["input_system"]); // Add this line
 
-	//let input_bundle = InputBundle::<String, String>::new().with_bindings_from_file(binding_path)?;
+    let mut game = Application::new("./", Board::new(10, 10), game_data)?;
+    game.run();
 
-	let game_data = GameDataBuilder::default()
-		.with_bundle(TransformBundle::new())?
-		.with_bundle(RenderBundle::new(pipe, Some(config)))?;
-		//.with_bundle(input_bundle)?
-		//.with(systems::PaddleSystem, "paddle_system", &["input_system"]); // Add this line
-
-	let mut game = Application::new("./", Board::new(15, 10), game_data)?;
-	game.run();
-
-	Ok(())
+    Ok(())
 }
