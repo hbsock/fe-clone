5973565d2d3ad03d53bd3e9b701357209675899b
Hanbin Sock <hbsock@gmail.com>
2018-09-25 23:07:22 -0400

    Check if horizontal cursor input can be read.

--- src/systems/cursor.rs
+++ src/systems/cursor.rs
@@ -15,7 +15,12 @@ impl<'s> System<'s> for CursorSystem {
 
   fn run(&mut self, (mut transforms, cursors, input): Self::SystemData) {
     for (cursor, transform) in (&cursors, &mut transforms).join() {
-        //println!("CursorSystem run!")
+        if let Some(x_move) = input.axis_value("cursor_horizontal") {
+            println!("x_move is: {}", x_move);
+        }
+
+        //let y_move = input.axis_value("cursor_vertical");
+
     }
   }
 }
